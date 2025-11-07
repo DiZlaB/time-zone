a5fb7a7665cfaf793c606d6a41f67e01e7b419c6
DiZlaB <dilyana.zabunova@gmail.com>
2025-11-07 13:36:02 +0100

    html
    - added new fake city info

--- src/script.js
+++ src/script.js
@@ -5,18 +5,18 @@ function selectCity(event) {
   if (event.target.value !== "") {
     if (event.target.value === "Melbourne") {
       let pickedCity = moment.tz("Australia/Melbourne").format("dddd, MMMM Do YYYY H:mm A ")
-let result = document.querySelector("#picked-city");
+let result = document.querySelector("#local-time");
 result.innerHTML = `It is ${pickedCity} in Melbourne, Australia`;
     }
     else if(event.target.value === "New Delhi") {
       let pickedCity = moment.tz("Asia/Kolkata").format("dddd, MMMM Do YYYY H:mm A")
-let result = document.querySelector("#picked-city");
+let result = document.querySelector("#local-time");
 result.innerHTML = `It is ${pickedCity} in New Delhi, India`;
     }
     else if (event.target.value === "Cape Town") 
      {
       let pickedCity = moment.tz("Africa/Johannesburg").format("dddd, MMMM Do YYYY H:mm A")
-let result= document.querySelector("#picked-city");
+let result= document.querySelector("#local-time");
 result.innerHTML = `It is ${pickedCity} in Cape Town, Africa`;
     }
   }}
