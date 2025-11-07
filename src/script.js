dd6bdf1d17766231da09e4c009305bb0b4192ea7
DiZlaB <dilyana.zabunova@gmail.com>
2025-11-07 21:08:35 +0100

    HTML & JS
    -added more options in select element
    - deleted the face data from HTML local time
    - deleted the old JS code  and created the the new one adjusting to the design in css; alsoit shows the local time zone, not the actual personal location though.

--- src/script.js
+++ src/script.js
@@ -1,25 +1,15 @@
-let city=document.querySelector("#city-dropdown")
-city.addEventListener ("change", selectCity)
+setInterval(() => {
+let yourLocation=document.querySelector ("#local-time")
+let localCity=yourLocation.querySelector(".city-Name")
+let localDate=yourLocation.querySelector(".date")
+let localTimeyZone=yourLocation.querySelector(".time")
 
-function selectCity(event) {
-  if (event.target.value !== "") {
-    if (event.target.value === "Melbourne") {
-      let pickedCity = moment.tz("Australia/Melbourne").format("dddd, MMMM Do YYYY H:mm A ")
-let result = document.querySelector("#local-time");
-result.innerHTML = `It is ${pickedCity} in Melbourne, Australia`;
-    }
-    else if(event.target.value === "New Delhi") {
-      let pickedCity = moment.tz("Asia/Kolkata").format("dddd, MMMM Do YYYY H:mm A")
-let result = document.querySelector("#local-time");
-result.innerHTML = `It is ${pickedCity} in New Delhi, India`;
-    }
-    else if (event.target.value === "Cape Town") 
-     {
-      let pickedCity = moment.tz("Africa/Johannesburg").format("dddd, MMMM Do YYYY H:mm A")
-let result= document.querySelector("#local-time");
-result.innerHTML = `It is ${pickedCity} in Cape Town, Africa`;
-    }
-  }}
+let zone = moment.tz.guess();                      
+let city = zone.split("/")[1].replace("_", " ");   
+localCity.innerHTML = city;
 
+localDate.innerHTML=moment.tz().format ("DD.MM.YYYY")
+localTimeyZone.innerHTML=moment.tz().format ("H:mm")
+},1000);
 
 
