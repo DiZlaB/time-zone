8aa75b8cf9d20ce52bcd4b1a03b964797a62129b
DiZlaB <dilyana.zabunova@gmail.com>
2025-11-08 12:16:11 +0100

    JS
    added the funtional time-zone/ date, time and city are being updated

--- src/script.js
+++ src/script.js
@@ -12,4 +12,25 @@ localDate.innerHTML=moment.tz().format ("DD.MM.YYYY")
 localTimeyZone.innerHTML=moment.tz().format ("H:mm")
 },1000);
 
+let worldTime=document.querySelector("#city-dropdown")
+worldTime.addEventListener("change", addNewTimeZone)
 
+
+function addNewTimeZone (event){
+let selectedCity=document.querySelector("#world-time")
+
+const cityTimezones = {
+  "Cape Town": "Africa/Johannesburg",
+  "Melbourne": "Australia/Melbourne",
+  "New Delhi": "Asia/Kolkata",
+  "Paris": "Europe/Paris",
+  "San Diego": "America/Los_Angeles",
+  "Sofia": "Europe/Sofia"
+};
+selectedCity.innerHTML=`
+        <div class="left-column">
+           <h5 class="city-Name">${event.target.value}</h5>
+          <div class="date"> ${moment.tz(cityTimezones[event.target.value]).format("DD.MM.YYYY")} </div>
+        </div>
+      <div class="time">${moment.tz(cityTimezones[event.target.value]).format("H:mm")}</div>`
+}
